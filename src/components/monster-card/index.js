44bfe71ce7a61128ed33e238436739c2af0081f4
mercspring <68706508+mercspring@users.noreply.github.com>
2020-11-30 17:13:04 -0800

    added the table of elemental weaknesses

--- src/components/monster-card/index.js
+++ src/components/monster-card/index.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Weaknesses from '../weakness-list'
+import './index.css'
 
 export default function MonsterCard(props) {
+    const [size, setSize] = useState("large")
+    function onClick() {
+        if (size === "large") {
+            setSize("small");
+        } else {
+            setSize("large")
+        }
+    }
     return (
         <div>
             <h1>Name: {props.name}</h1>
             <h2>{props.img}</h2>
-            <img src={process.env.PUBLIC_URL + "/img/monsters/" + props.img}></img>
+            <div className="wrapper">
+                <span className={size === "large" ? "name" : "view-name"}>{props.name}</span>
+                <Weaknesses/>
+                {/* <div className="image-background">
+                    <img onClick={onClick} className={size === "large" ? null : "clicked"} src={process.env.PUBLIC_URL + "/img/monsters/" + props.img}></img>
+                </div> */}
+            </div>
         </div>
     )
 }
---
+++ src/components/weakness-list/index.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function Weaknesses() {
+    return (
+        <div className="Weaknesses">
+            <table>
+                <tr>
+                    <td className="fire icon"></td>
+                    <td>⭐⭐⭐</td>
+                </tr>
+                <tr>
+                    <td className="water icon"></td>
+                    <td>⭐⭐⭐</td>
+                </tr>
+                <tr>
+                    <td className="thunder icon"></td>
+                    <td>⭐⭐⭐</td>
+                </tr>
+                <tr>
+                    <td className="ice icon"></td>
+                    <td>⭐⭐⭐</td>
+                </tr>
+                <tr>
+                    <td className="dragon icon"></td>
+                    <td>⭐⭐⭐</td>
+                </tr>
+            </table>
+        </div>
+    )
+}
