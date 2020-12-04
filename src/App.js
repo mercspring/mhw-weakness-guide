f52e1b0ddaf39153b3693fecad34f7ba044ae070
mercspring <68706508+mercspring@users.noreply.github.com>
2020-12-03 16:53:26 -0800

    updated package-lock.json

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import MonsterCard from './components/monster-card'
-import { Monsters } from './data/monsters.js'
+import MonsterCard from './components/monster-card';
+import { Monsters } from './data/monsters.js';
 import "./index.css";
 function App() {
   //  const monsters = Monsters; 
@@ -8,10 +8,9 @@ function App() {
     <div className="App">
       <div style={{display: "flex", flexWrap: "wrap", width:"95vw", margin:"auto 0", justifyContent: "center"}}>
         {Monsters.map((elm, index) => {
-          return <MonsterCard monsterInfo={elm} img={elm.icon} name={elm.attributes.name} />
+          return <MonsterCard key={index} monsterInfo={elm} img={elm.icon} name={elm.attributes.name} />
         })}
       </div>
-      {/* <MonsterCard monsterInfo = {Monsters[1]}img={Monsters[1].icon} name={Monsters[1].attributes.name}/> */}
     </div>
   );
 }
--- src/components/weakness-list/index.js
+++ src/components/weakness-list/index.js
@@ -2,62 +2,67 @@ import React from 'react'
 import './style.css'
 
 export default function Weaknesses(props) {
-    function setPhysiology(weakness){
-        if(weakness === 3){
+    function setPhysiology(weakness) {
+        if (weakness === 3) {
             return "⭐⭐⭐"
-        } else if(weakness === 2 ){
+        } else if (weakness === 2) {
             return "⭐⭐"
-        } else if(weakness === 1 ){
+        } else if (weakness === 1) {
             return "⭐"
-        } else if(weakness === 0){
+        } else if (weakness === 0) {
             return "❌"
         }
     }
     return (
         <div className={props.hidden ? "Weaknesses table hide-table" : "Weaknesses table view-table"}>
             <table className="element-table">
-                <tr>
-                    <td className="fire icon"></td>
-                    <td>{setPhysiology(props.monsterInfo.attributes.physiology.fire)}</td>
-                </tr>
-                <tr>
-                    <td className="water icon"></td>
-                    <td>{setPhysiology(props.monsterInfo.attributes.physiology.water)}</td>
-                </tr>
-                <tr>
-                    <td className="thunder icon"></td>
-                    <td>{setPhysiology(props.monsterInfo.attributes.physiology.thunder)}</td>
-                </tr>
-                <tr>
-                    <td className="ice icon"></td>
-                    <td>{setPhysiology(props.monsterInfo.attributes.physiology.ice)}</td>
-                </tr>
-                <tr>
-                    <td className="dragon icon"></td>
-                    <td>{setPhysiology(props.monsterInfo.attributes.physiology.dragon)}</td>
-                </tr>
+                <tbody>
+                    <tr>
+                        <td className="fire icon"></td>
+                        <td>{setPhysiology(props.monsterInfo.attributes.physiology.fire)}</td>
+                    </tr>
+                    <tr>
+                        <td className="water icon"></td>
+                        <td>{setPhysiology(props.monsterInfo.attributes.physiology.water)}</td>
+                    </tr>
+                    <tr>
+                        <td className="thunder icon"></td>
+                        <td>{setPhysiology(props.monsterInfo.attributes.physiology.thunder)}</td>
+                    </tr>
+                    <tr>
+                        <td className="ice icon"></td>
+                        <td>{setPhysiology(props.monsterInfo.attributes.physiology.ice)}</td>
+                    </tr>
+                    <tr>
+                        <td className="dragon icon"></td>
+                        <td>{setPhysiology(props.monsterInfo.attributes.physiology.dragon)}</td>
+                    </tr>
+                </tbody>
             </table>
             <table className="status-table">
-                <tr>
-                    <td className="poison icon"></td>
-                    <td>{setPhysiology(props.monsterInfo.attributes.physiology.poison)}</td>
-                </tr>
-                <tr>
-                    <td className="sleep icon"></td>
-                    <td>{setPhysiology(props.monsterInfo.attributes.physiology.sleep)}</td>
-                </tr>
-                <tr>
-                    <td className="paralysis icon"></td>
-                    <td>{setPhysiology(props.monsterInfo.attributes.physiology.paralysis)}</td>
-                </tr>
-                <tr>
-                    <td className="blast icon"></td>
-                    <td>{setPhysiology(props.monsterInfo.attributes.physiology.blast)}</td>
-                </tr>
-                <tr>
-                    <td className="stun icon"></td>
-                    <td>{setPhysiology(props.monsterInfo.attributes.physiology.stun)}</td>
-                </tr>
+                <tbody>
+                    <tr>
+                        <td className="poison icon"></td>
+                        <td>{setPhysiology(props.monsterInfo.attributes.physiology.poison)}</td>
+                    </tr>
+                    <tr>
+                        <td className="sleep icon"></td>
+                        <td>{setPhysiology(props.monsterInfo.attributes.physiology.sleep)}</td>
+                    </tr>
+                    <tr>
+                        <td className="paralysis icon"></td>
+                        <td>{setPhysiology(props.monsterInfo.attributes.physiology.paralysis)}</td>
+                    </tr>
+                    <tr>
+                        <td className="blast icon"></td>
+                        <td>{setPhysiology(props.monsterInfo.attributes.physiology.blast)}</td>
+                    </tr>
+                    <tr>
+                        <td className="stun icon"></td>
+                        <td>{setPhysiology(props.monsterInfo.attributes.physiology.stun)}</td>
+                    </tr>
+                </tbody>
+
             </table>
         </div>
     )
