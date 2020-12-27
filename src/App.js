1c39602efa16e0e4953112b7faa854796ceced71
mercspring <68706508+mercspring@users.noreply.github.com>
2020-12-27 12:56:22 -0800

    added search funtionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import {React, useState} from 'react';
 import MonsterCard from './components/monster-card';
 import SearchBar from './components/search-bar';
 import Header from './components/header';
@@ -6,11 +6,12 @@ import { Monsters } from './data/monsters.js';
 import "./index.css";
 function App() {
   //  const monsters = Monsters; 
+const [monsterList, setMonsterList] = useState(Monsters)
   return (
     <div className="App">
-      <Header/>
+      <Header setMonsterList={setMonsterList} Monsters={Monsters}/>
       <div style={{ display: "flex", flexWrap: "wrap", width: "95vw", justifyContent:"center"}}>
-        {Monsters.map((elm, index) => {
+        {monsterList.map((elm, index) => {
           return <MonsterCard key={index} monsterInfo={elm} img={elm.icon} name={elm.attributes.name} />
         })}
       </div>
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import {React, useState} from 'react'
 const styles = {
     container: {
         display: "flex",
@@ -9,13 +9,29 @@ const styles = {
     }
 }
 
-export default function Header() {
+export default function Header(props) {
+    const [filterTerm, setFilterTerm] = useState('')
+
+    function onFilterChange(event){
+        const value = event.target.value;
+        const re = new RegExp(value, 'i');
+
+       const monsters = props.Monsters.filter(elm =>{
+            return re.test(elm.attributes.name)
+        });
+
+        props.setMonsterList(monsters);
+        setFilterTerm(value)
+
+
+    }
+
     return (
         <div className="search-container">
             <h1 className="site-name">Monster Hunter Weakness Guide</h1>
             <div>
                 <label for="search-bar" className="search-label"> Filter </label>
-                <input id="serach-bar" type="text" className="search-bar"></input>
+                <input id="serach-bar" type="text" onChange={onFilterChange} className="search-bar" value={filterTerm}></input>
             </div>
         </div>
     )
